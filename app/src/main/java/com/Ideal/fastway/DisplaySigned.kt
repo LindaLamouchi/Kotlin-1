a12d7393cc4b9c4481d78cc7aa7b2064759c1c1e
Linda <lamouchilinda@gmail.com>
2021-03-18 18:04:21 +0100

    Delete and Add in a random Position

--- app/src/main/java/com/Ideal/fastway/DisplaySigned.kt
+++ app/src/main/java/com/Ideal/fastway/DisplaySigned.kt
@@ -2,21 +2,40 @@ package com.Ideal.fastway
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_display_signed.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class DisplaySigned : AppCompatActivity() {
-
+   private val exampleList=generateDummyList(500)
+    private val adapter=ExampleAdapter(exampleList)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_signed)
 
-        val exampleList=generateDummyList(500)
-        recycler_view.adapter=ExampleAdapter(exampleList)
+
+        recycler_view.adapter=adapter
         recycler_view.layoutManager= LinearLayoutManager(this)
         recycler_view.setHasFixedSize(true)
     }
-    private fun generateDummyList(size: Int): List<ExampleItem> {
+    fun addItem(view:View){
+    val index= Random.nextInt(8)
+        val newItem=ExampleItem(
+            R.drawable.ic_file,
+            "New item at position $index",
+            "Line 2"
+        )
+        exampleList.add(index,newItem)
+        adapter.notifyItemInserted(index)
+    }
+    fun deleteItem(view:View){
+        val index = Random.nextInt(8)
+        exampleList.removeAt(index)
+        adapter.notifyItemRemoved(index)
+    }
+    private fun generateDummyList(size: Int): ArrayList<ExampleItem> {
         val list = ArrayList<ExampleItem>()
         for (i in 0 until size) {
             val drawable = when (i % 3) {
